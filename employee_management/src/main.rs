35c4acae9d9b6d58e5b1687193e3bf2116f4e84b
Kevin Van Der Werff <Kevinsebastianate@hotmail.com>
2018-07-10 03:20:14 +0200

    init employee_management

---
+++ employee_management/src/main.rs
@@ -0,0 +1,14 @@
+/**
+ * Chapter 8, Common Collections
+ * https://doc.rust-lang.org/book/second-edition/ch08-03-hash-maps.html#creating-a-new-hash-map
+ * 
+ * Using a hash map and vectors, create a text interface
+ * to allow a user to add employee names to a department in a company. 
+ * For example, “Add Sally to Engineering” or “Add Amir to Sales".
+ * Then let the user retrieve a list of all people in a department
+ * or all people in the company by department, sorted alphabetically.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
