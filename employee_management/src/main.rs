49050c72310730c97359970f8c78bfb8e934b418
Kevin Van Der Werff <Kevinsebastianate@hotmail.com>
2018-07-10 16:11:35 +0200

    Read in input with error handling

--- employee_management/src/main.rs
+++ employee_management/src/main.rs
@@ -9,6 +9,53 @@
  * or all people in the company by department, sorted alphabetically.
  */
 
+use std::collections::HashMap;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut employees: HashMap<&str, &str> = HashMap::new();
+    
+    loop {
+        println!("");
+        println!("Welcome! Which action do you want to perform?");
+        println!("1. View all employees of the company.");
+        println!("2. View all employees of chosen department.");
+        println!("3. Add an employee.");
+        println!("");
+
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input)
+            .expect("Failed to read line");
+
+        let mut input = match input.trim().parse::<i32>() {
+            Ok(value) => value,
+            Err(_) => {
+                println!("Please put in a number");
+                continue;
+            },
+        };
+
+        match input {
+             1 => employeesByDepartment(),
+             2 => allEmployees(),
+             3 => addEmployee(),
+             _ => {
+                println!("Please choose one of the options");
+                continue;
+            }
+        }
+    }
+}
+
+fn employeesByDepartment() {
+
+}
+
+fn allEmployees() {
+
+}
+
+fn addEmployee() {
+
 }
