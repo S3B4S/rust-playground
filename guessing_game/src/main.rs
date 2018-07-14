154c3477f07a787647506d51ad078e5537773575
Unknown <Kevinsebastianate@hotmail.com>
2018-07-14 02:50:53 +0200

    Add panic test for guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -51,3 +51,14 @@ fn main() {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be between 1 and 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
